464683935fe2516dc931beed7087582463347196
Trevor <tlarnold10@hotmail.com>
2023-03-30 15:35:10 -0500

    created employee list and details pages

---
+++ managementTool/src/routes/employee/+page.server.ts
@@ -0,0 +1,14 @@
+import type { EmployeeModel } from "../../../../../../managementTool/managementTool/managementTool/src/models/employee.model";
+import employees from '../../../static/mock-data/mockEmployees.json';
+
+export async function load({params}) {
+    // const response = fetch('./mock-data/mockEmployees.json');
+    return {
+        employees: employees.map((employee) => (
+            {
+                name: employee.name,
+                position: employee.position
+            }
+        ))
+    }
+}
