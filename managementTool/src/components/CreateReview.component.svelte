2de018bda721ec557e302cd3ec2ae36a0f5052a9
Trevor <tlarnold10@hotmail.com>
2023-05-10 11:48:52 -0500

    finished up creation of review page

--- managementTool/src/components/CreateReview.component.svelte
+++ managementTool/src/components/CreateReview.component.svelte
@@ -3,12 +3,26 @@
     import type { EmployeeModel } from "../models/employee.model";
     import type { FeedbackModel } from "../models/feedback.model";
     import type { OneOnOneModel } from "../models/one-on-one.model";
-    export var data;
+    import type { EmployeeReviewModel } from "../models/review.model";
+    export var data: any;
 
     var _currentEmployee: EmployeeModel = data?.currentEmployee;
 
     let feedbackItems: FeedbackModel[] = [];
     let oneOnOnes: OneOnOneModel[] = [];
+    let reviews: EmployeeReviewModel[] = [];
+    
+
+    export var newReview: EmployeeReviewModel = {
+        name: '',
+        reviewDate: new Date(),
+        employeeReviewFeedback: ''
+    }
+    newReview;
+    $: reviews;
+    $: if (_currentEmployee !== undefined && _currentEmployee.employeeReviews !== undefined && newReview.employeeReviewFeedback != '') {
+        reviews = [...reviews, newReview];
+    }
 
     if (_currentEmployee !== undefined && _currentEmployee.employeeFeedback !== undefined) {
         _currentEmployee.employeeFeedback.forEach(feedback => {
@@ -22,9 +36,22 @@
         });
     }
 
+if (_currentEmployee !== undefined && _currentEmployee.employeeReviews !== undefined) {
+    _currentEmployee.employeeReviews.forEach(review => {
+        reviews.push(review);
+    });
+}
+
     let selectedItemId: any = null;
     function selectedItem(id: number) {
         selectedItemId = id
+    }
+
+    function onSubmit(e: any) {
+        var reviewString: string = `Period Summary: ${e.target[0].value} \nKudos: ${e.target[1].value} \nAreas for Improvement: ${e.target[2].value} \nAction Items: ${e.target[3].value}`;
+        
+        newReview.name = _currentEmployee.name;
+        newReview.employeeReviewFeedback = reviewString;
     }
 
 </script>
@@ -32,15 +59,19 @@
 <style>
     .container {
         display: flex;
-        height: 100vh;
     }
     .list {
         flex: 1;
         padding: 1rem;
+        border: 1px solid black;
     }
     .form {
         flex: 1;
         padding: 1rem;
+        border: 1px solid black;
+    }
+    .details {
+        color: black;
     }
 </style>
 
@@ -67,10 +98,50 @@
         {/each}
     </div>
     <div class="form">
-        {#if selectedItem}
-            <h2>Form from {selectedItem}</h2>
-        {:else}
-            <p>No item selected</p>
-        {/if}
+        <form on:submit|preventDefault={onSubmit}>
+			<div class="my-5">
+                <label for="feedbackArea">Period Summary: </label>
+                <textarea class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                id="feedbackArea"
+                rows="4"
+                placeholder="Your message"></textarea>
+                <br>
+                <label for="feedbackArea">Kudos: </label>
+                <textarea class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                id="feedbackArea"
+                rows="4"
+                placeholder="Your message"></textarea>
+                <br>
+                <label for="feedbackArea">Areas for Improvement: </label>
+                <textarea class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                id="feedbackArea"
+                rows="4"
+                placeholder="Your message"></textarea>
+                <br>
+                <label for="feedbackArea">Action Items: </label>
+                <textarea class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                id="feedbackArea"
+                rows="4"
+                placeholder="Your message"></textarea>
+			</div>
+			<!--Footer-->
+			<div class="flex justify-end pt-2">
+				<button
+					class="focus:outline-none modal-close px-4 bg-gray-400 p-3 rounded-lg text-black hover:bg-gray-300" type="reset">Cancel</button>
+				<button
+					class="focus:outline-none px-4 bg-teal-500 p-3 ml-3 rounded-lg text-white hover:bg-teal-400" type="submit">Confirm</button>
+			</div>
+            </form>
     </div>
+</div>
+<div>
+    <h2>Previous Reviews for reference</h2>
+    <dl class="text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700">
+    {#each reviews as review}
+        <div class="flex flex-col pb-3">
+            <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">{review.reviewDate.toDateString()}</dt>
+            <dd class="text-lg font-semibold details">{review.employeeReviewFeedback}</dd>
+        </div>
+    {/each}
+    </dl>
 </div>
